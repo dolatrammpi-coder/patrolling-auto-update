a5588c277914ed05874fb255a1369c91c29a3954
dolatrammpi-coder <dolatrammpi@gmail.com>
2026-01-18 20:43:54 +0530

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -94,8 +94,8 @@ try:
     # ===============================
     REPORT_URL = (
         "https://ip3.rilapp.com/railways/patrollingReport.php"
-        "?fdate=17/01/2026&ftime=04:00"
-        "&tdate=18/01/2026&ttime=16:00"
+        "?fdate=17/01/2026&ftime=23:00"
+        "&tdate=18/01/2026&ttime=7:10"
         "&category=-PM&Submit=Update"
     )
 
